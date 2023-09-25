0c1e9f2a2973c3245053eddb45d2098b68ce2df0
Sam Alws <sam@samalws.com>
2023-09-25 19:21:21 -0400

    gather is p much done

--- Compiler/Typechecker/Gather.hs
+++ Compiler/Typechecker/Gather.hs
@@ -10,40 +10,75 @@ import qualified Data.Map as M
 import Control.Monad (when)
 import Data.Maybe (isJust, fromJust)
 
-gatherNSConstraint :: (GatherMonad m) => StmtID -> (VarID, String, ProcType) -> m ()
-gatherNSConstraint = undefined -- TODO
+-- ptb cant have any constraints that pta doesnt have
+pushPtsGeq :: (GatherMonad m) => ProcType -> ProcType -> m ()
+pushPtsGeq pta ptb = do
+  let tmpa = pta.procTypeTemplate
+  let tmpb = ptb.procTypeTemplate
+  when (length tmpa.typeArgs /= length tmpb.typeArgs || length tmpa.nsArgs /= length tmpb.nsArgs || length pta.procTypeArgs /= length ptb.procTypeArgs) $ fail "mismatched procs: wrong template/arg length"
 
-getProcType :: (GatherMonad m) => StmtID -> Stmt -> m ProcType
-getProcType sid pc@(ProcCall{}) = do
-  globalNsVal <- M.lookup pc.callNS . globalNamespaces <$> getGlobalEnv
+  let typesPaired = M.fromList $ tmpa.typeArgs `zip` (VarType <$> tmpb.typeArgs)
+  let nssPaired = M.fromList $ tmpa.nsArgs `zip` tmpb.nsArgs
+  let argsPaired = (snd <$> pta.procTypeArgs) `zip` (snd <$> ptb.procTypeArgs)
+
+  let typeApplyRewriteHere = typeApplyRewrite typesPaired nssPaired
+  let nsApplyRewriteHere = nsApplyRewrite typesPaired nssPaired
+  let procTypeApplyRewriteHere = procTypeApplyRewrite typesPaired nssPaired
+
+  mapM_ (\(ta, tb) -> typeApplyRewriteHere ta `pushEqConstraint` tb) argsPaired
+
+  let numConstrsA' = S.map (typeApplyRewriteHere . VarType) tmpa.numericConstraints
+  let numConstrsB' = S.map VarType tmpb.numericConstraints
+  let intConstrsA' = S.map (typeApplyRewriteHere . VarType) tmpa.intConstraints
+  let intConstrsB' = S.map VarType tmpb.intConstraints
+  let
+    modifyNsConstr :: (VarID, String, ProcType) -> (VarID, String, Template, [Type])
+    modifyNsConstr (n,f,t) = (n,f,t.procTypeTemplate,snd <$> t.procTypeArgs) -- we want to ignore type names when checking equality
+  let nsConstrsA' = S.map (modifyNsConstr . (\(n,f,t) -> (nsApplyRewriteHere n, f, procTypeApplyRewriteHere t))) tmpa.nsConstraints
+  let nsConstrsB' = S.map modifyNsConstr tmpb.nsConstraints
+
+  when (not $ S.null $ S.difference numConstrsB' numConstrsA') $ fail "mismatched procs: numeric constraints not matched"
+  when (not $ S.null $ S.difference intConstrsB' intConstrsA') $ fail "mismatched procs: int constraints not matched"
+  when (not $ S.null $ S.difference nsConstrsB' nsConstrsA') $ fail "mismatched procs: namespace constraints not matched"
+
+gatherNSConstraint :: (GatherMonad m) => (VarID, String, ProcType) -> m ()
+gatherNSConstraint (ns, fn, pt) = pushPtsGeq pt =<< getProcType ns fn
+
+getProcType :: (GatherMonad m) => VarID -> String -> m ProcType
+getProcType ns fn = do
+  globalNsVal <- M.lookup ns . globalNamespaces <$> getGlobalEnv
   let isGlobal = isJust globalNsVal
 
   thisProcTemplate <- procTypeTemplate . procType <$> getProc
   let localNsConstrs = thisProcTemplate.nsConstraints
-  let isLocal = pc.callNS `elem` thisProcTemplate.nsArgs
+  let isLocal = ns `elem` thisProcTemplate.nsArgs
 
   if isGlobal then do
-    let Just p = pc.callFn `M.lookup` fns (fromJust globalNsVal)
+    let Just p = fn `M.lookup` fns (fromJust globalNsVal)
     pure p.procType
   else if isLocal then do
-    let [(_,_,t)] = filter (\(n,f,_) -> n == pc.callNS && f == pc.callFn) $ S.toList localNsConstrs -- TODO MonadFail handles this correctly?
+    let [(_,_,t)] = filter (\(n,f,_) -> n == ns && f == fn) $ S.toList localNsConstrs -- TODO MonadFail handles this correctly?
     pure t
   else fail "ProcCall with nonexistent namespace"
-
-getProcType _ _ = error "getProcType called with non ProcCall value"
 
 gatherProcCall :: (GatherMonad m) => StmtID -> Stmt -> ProcType -> m () 
 gatherProcCall sid pc@(ProcCall{}) pt = do
   when (length pc.callTypeArgs /= length pt.procTypeTemplate.typeArgs || length pc.callNSArgs /= length pt.procTypeTemplate.nsArgs || length pc.callArgs /= length pt.procTypeArgs) $ fail "incorrect number of proc arguments"
   let typesPaired = M.fromList $ pt.procTypeTemplate.typeArgs `zip` pc.callTypeArgs
   let nssPaired = M.fromList $ pt.procTypeTemplate.nsArgs `zip` pc.callNSArgs
-  let typeApplyTemplateHere = typeApplyTemplate typesPaired nssPaired
-  let expectedArgTypes = typeApplyTemplateHere . snd <$> pt.procTypeArgs
+
+  let typeApplyRewriteHere = typeApplyRewrite typesPaired nssPaired
+  let nsApplyRewriteHere = nsApplyRewrite typesPaired nssPaired
+  let procTypeApplyRewriteHere = procTypeApplyRewrite typesPaired nssPaired
+
+  let expectedArgTypes = typeApplyRewriteHere . snd <$> pt.procTypeArgs
   realArgTypes <- mapM (getVarTypeAt sid) pc.callArgs
+
   sequence_ $ zipWith pushEqConstraint realArgTypes expectedArgTypes
-  mapM_ (pushNumericConstraint . typeApplyTemplateHere . VarType) $ S.toList pt.procTypeTemplate.numericConstraints
-  mapM_ (pushIntConstraint . typeApplyTemplateHere . VarType) $ S.toList pt.procTypeTemplate.intConstraints
-  mapM_ (gatherNSConstraint sid . (\(n,f,t) -> (nsApplyTemplate typesPaired nssPaired n, f, procTypeApplyTemplate typesPaired nssPaired t))) $ S.toList pt.procTypeTemplate.nsConstraints
+
+  mapM_ (pushNumericConstraint . typeApplyRewriteHere . VarType) $ S.toList pt.procTypeTemplate.numericConstraints
+  mapM_ (pushIntConstraint . typeApplyRewriteHere . VarType) $ S.toList pt.procTypeTemplate.intConstraints
+  mapM_ (gatherNSConstraint . (\(n,f,t) -> (nsApplyRewriteHere n, f, procTypeApplyRewriteHere t))) $ S.toList pt.procTypeTemplate.nsConstraints
 
 gatherProcCall _ _ _ = error "gatherProcCall called with non ProcCall value"
 
@@ -62,7 +97,7 @@ gatherStmt_ sid (AssignMember v r m _) = do
   pushMemberTypeConstraint tr m tm
   pushGeqConstraint tv tm
 gatherStmt_ sid pc@(ProcCall{}) = do
-  pt <- getProcType sid pc
+  pt <- getProcType pc.callNS pc.callFn
   gatherProcCall sid pc pt
 gatherStmt_ sid (JNZ v _ _) = do
   tv <- getVarTypeAt sid v
--- Compiler/Typechecker/Types.hs
+++ Compiler/Typechecker/Types.hs
@@ -14,27 +14,27 @@ data Stmt = AssignVar VarID VarID StmtID
           | JNZ VarID {- where you go if nonzero: -} StmtID {- where you go if zero: -} StmtID
           | AssertVarType VarID Type StmtID
           | Return
-          deriving (Show, Eq)
+          deriving (Show, Eq, Ord)
 data Literal = IntLiteral { litIsSigned :: Bool, litBits :: Int, litIntVal :: Integer }
              | FloatLiteral Float
              | DoubleLiteral Double
              | TagLiteral Tag
-             deriving (Show, Eq)
+             deriving (Show, Eq, Ord)
 data Type = IntType { signed :: Bool, bits :: Int }
           | FloatType
           | DoubleType
           | TagType Tag
           | RecordsType [[RecordEntryType]]
           | VarType VarID
-          deriving (Show, Eq)
-data Template = Template { typeArgs :: [VarID], nsArgs :: [VarID], numericConstraints :: S.Set VarID, intConstraints :: S.Set VarID, nsConstraints :: S.Set (VarID, String, ProcType) } deriving (Show, Eq)
+          deriving (Show, Eq, Ord)
+data Template = Template { typeArgs :: [VarID], nsArgs :: [VarID], numericConstraints :: S.Set VarID, intConstraints :: S.Set VarID, nsConstraints :: S.Set (VarID, String, ProcType) } deriving (Show, Eq, Ord)
 -- TODO ideally namespaces should be templates too, but I don't feel like filling in the logic for that
 -- (once you do this, have to modify getProcType as well as the defn of NamespaceValue and ProcCall)
-data NamespaceValue = NSValue { fns :: M.Map String Proc } deriving (Show, Eq)
-data RecordEntryType = RecordEntryType Type (Maybe RecordEntryConstraint) deriving (Show, Eq)
-data RecordEntryConstraint = EqConstraint Literal | RangeConstraint Literal Literal deriving (Show, Eq)
-data ProcType = ProcType { procTypeTemplate :: Template, procTypeArgs :: [(VarID, Type)] } deriving (Show, Eq)
-data Proc = Proc { procType :: ProcType, procStmts :: M.Map StmtID Stmt } deriving (Show, Eq) -- 0 is entry point, not that it matters
+data NamespaceValue = NSValue { fns :: M.Map String Proc } deriving (Show, Eq, Ord)
+data RecordEntryType = RecordEntryType Type (Maybe RecordEntryConstraint) deriving (Show, Eq, Ord)
+data RecordEntryConstraint = EqConstraint Literal | RangeConstraint Literal Literal deriving (Show, Eq, Ord)
+data ProcType = ProcType { procTypeTemplate :: Template, procTypeArgs :: [(VarID, Type)] } deriving (Show, Eq, Ord)
+data Proc = Proc { procType :: ProcType, procStmts :: M.Map StmtID Stmt } deriving (Show, Eq, Ord) -- 0 is entry point, not that it matters
 
 stmtSuccessors :: Stmt -> S.Set StmtID
 stmtSuccessors (AssignVar _ _ id) = S.singleton id
@@ -51,11 +51,11 @@ litType (FloatLiteral _) = FloatType
 litType (DoubleLiteral _) = DoubleType
 litType (TagLiteral t) = TagType t
 
-typeApplyTemplate :: M.Map VarID Type -> M.Map VarID VarID -> Type -> Type
-typeApplyTemplate = undefined -- TODO
+typeApplyRewrite :: M.Map VarID Type -> M.Map VarID VarID -> Type -> Type
+typeApplyRewrite = undefined -- TODO
 
-nsApplyTemplate :: M.Map VarID Type -> M.Map VarID VarID -> VarID -> VarID
-nsApplyTemplate = undefined -- TODO
+nsApplyRewrite :: M.Map VarID Type -> M.Map VarID VarID -> VarID -> VarID
+nsApplyRewrite = undefined -- TODO
 
-procTypeApplyTemplate :: M.Map VarID Type -> M.Map VarID VarID -> ProcType -> ProcType
-procTypeApplyTemplate = undefined -- TODO
+procTypeApplyRewrite :: M.Map VarID Type -> M.Map VarID VarID -> ProcType -> ProcType
+procTypeApplyRewrite = undefined -- TODO
